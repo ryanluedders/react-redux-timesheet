4066e7ac6104867c260a2fdb864ff6493eb6bffe
Ryan Luedders <ryan.luedders@gmail.com>
2018-06-12 11:55:15 -0500

    additional extra credit and cleanup

--- lab-02-first-component/src/App.test.js
+++ lab-02-first-component/src/App.test.js
@@ -2,9 +2,18 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import App from './App';
+import Hello from './hello/Hello';
 
 describe('App Component', () => {
+
   it('renders without exploding', () => {
     expect(shallow(<App />)).toHaveLength(1);
   });
+
+  it('renders the Hello component', () => {
+    const app = shallow(<App />);
+
+    expect(app).toContainReact(<Hello friend="Woohoo!" />);
+  })
+
 });
--- lab-02-first-component/src/hello/Hello.test.js
+++ lab-02-first-component/src/hello/Hello.test.js
@@ -4,29 +4,31 @@ import renderer from 'react-test-renderer';
 
 import Hello from './Hello';
 
-describe('Hello World:', () => {});
+describe('Hello World:', () => {
 
-it('renders without exploding', () => {
-    expect(shallow(<Hello/>)).toHaveLength(1);
+    it('renders without exploding', () => {
+        expect(shallow(<Hello />)).toHaveLength(1);
+    });
+
+    it('should render with default test', () => {
+        const component = shallow(<Hello />);
+
+        expect(component).toIncludeText('Howdy');
+        expect(component).toIncludeText('Partner');
+    });
+
+    it('should render with our props', () => {
+        const component = shallow(<Hello friend="Fred" />);
+
+        expect(component).toIncludeText('Howdy');
+        expect(component).toIncludeText('Fred');
+        expect(component).not.toIncludeText('Partner');
+    })
+
+    it('renders to match a snapshot', () => {
+        const component = renderer.create(<Hello friend="Obi Wan" />);
+
+        expect(component.toJSON()).toMatchSnapshot();
+    })
+
 });
-
-it('should render with default test', () => {
-    const component = shallow(<Hello/>);
-    
-    expect(component).toIncludeText('Howdy');
-    expect(component).toIncludeText('Partner');
-});
-
-it ('should render with our props', () => {
-    const component = shallow(<Hello friend="Fred" />);
-
-    expect(component).toIncludeText('Howdy');
-    expect(component).toIncludeText('Fred');
-    expect(component).not.toIncludeText('Partner');
-})
-
-it ('renders to match a snapshot', () => {
-    const component = renderer.create(<Hello friend="Obi Wan" />);
-
-    expect(component.toJSON()).toMatchSnapshot();
-})
